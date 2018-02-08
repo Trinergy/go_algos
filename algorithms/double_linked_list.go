2559a7c55c10afdcb55fc585cb827c0b3293132c
Kenny Wu <kenny.wu@lendesk.com>
2018-02-08 08:05:05 -0800

    Add parentDLL with length

--- algorithms/double_linked_list.go
+++ algorithms/double_linked_list.go
@@ -4,20 +4,32 @@ import (
 	"fmt"
 )
 
+type ArrayDLL struct {
+	length int
+}
+
 type DLL struct {
 	value    string
 	previous *DLL
 	next     *DLL
+	parent   *ArrayDLL
 }
 
 func (dll *DLL) add(value string) {
-	nextDLL := DLL{value: value, previous: dll}
+	parentDLL := dll.parent
+	nextDLL := DLL{value: value, previous: dll, parent: parentDLL}
 	dll.next = &nextDLL
+
+	parentDLL.length++
 }
 
 func (dll *DLL) remove() {
 	previousDLL := dll.previous
 	nextDLL := dll.next
+
+	parentDLL := dll.parent
+	parentDLL.length--
+	parentDLL = nil
 
 	if previousDLL.previous == dll { // removing from head
 		previousDLL.previous = nil
@@ -27,7 +39,9 @@ func (dll *DLL) remove() {
 }
 
 func main() {
-	headDLL := DLL{value: "chicken"}
+	arrayDLL := ArrayDLL{length: 1} // assumes to be initiated with a child (bad)
+	// currently child also assumes knowledge of parent
+	headDLL := DLL{value: "chicken", parent: &arrayDLL}
 	headDLL.add("beef")
 	nextDLL := headDLL.next
 	headDLL.previous = nextDLL
@@ -35,9 +49,11 @@ func main() {
 	fmt.Printf("verify add: \nthis is first value: %q", headDLL.value)
 	fmt.Printf("\nthis is the second value: %q", nextDLL)
 	fmt.Printf("\nthis is the previous DLL: %q", headDLL.previous)
+	fmt.Printf("\nthis is the arrayDLL's length: %d", arrayDLL.length)
 
 	nextDLL.remove()
 
 	fmt.Printf("\nverify remove:")
 	fmt.Printf("\n this is the DLL after removal: %q", headDLL)
+	fmt.Printf("\nthis is the arrayDLL's length: %d", arrayDLL.length)
 }
